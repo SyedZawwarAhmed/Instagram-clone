89bf38ff62ead68a6dac1605d0b1d9faebc41349
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2021-10-18 00:18:13 +0500

    Suggestions component added

--- components/Feed.js
+++ components/Feed.js
@@ -1,6 +1,8 @@
 import React from "react";
+import MiniProfile from "./MiniProfile";
 import Posts from "./Posts";
 import Stories from "./Stories";
+import Suggestions from "./Suggestions";
 
 function Feed() {
   return (
@@ -10,7 +12,12 @@ function Feed() {
         <Posts />
       </section>
 
-      <section></section>
+      <section className="hidden xl:inline-grid md:col-span-1">
+        <div className="fixed top-20">
+          <MiniProfile />
+          <Suggestions />
+        </div>
+      </section>
     </main>
   );
 }
---
+++ components/MiniProfile.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function MiniProfile() {
+  return (
+    <div className="flex items-center justify-between mt-14 ml-10">
+      <img
+        className="w-16 h-16 rounded-full border p-[2px]"
+        src="https://links.papareact.com/3ke"
+        alt=""
+      />
+
+      <div className="flex-1 mx-4">
+        <h2 className='font-bold'>My name</h2>
+        <h3 className="text-gray-400">Welcome to Instagram</h3>
+      </div>
+
+      <button className="text-blue-400 text-sm font-semibold">Sign Out</button>
+    </div>
+  );
+}
+
+export default MiniProfile;
